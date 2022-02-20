8fd7d1ffb70ccee622dec858fde65dc43e48a043
Leif Arne Rones <leif.arne.rones@gmail.com>
2022-02-20 19:55:03 +0100

    x

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,17 @@
 const functions = require("firebase-functions");
+const fetch = require("node-fetch");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then(res => res.text())
+    .then(function (text) {
+         console.log(text);
+        return response.send('A message *with some bold text* and _some italicized text_.' + text);
+      });
 });
 
 // firebase functions:config:set slack.signing_secret=d02b377073eae11176076b4126fe8042
